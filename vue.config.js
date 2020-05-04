640fe7a2c7184b1bc2f9e467abb73cff98ca96a3
Felix Spöttel <1682504+fspoettel@users.noreply.github.com>
2020-05-04 19:41:54 +0200

    Add favicons

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  // ...other vue-cli plugin options...
+  pwa: {
+    name: 'BPM Counter',
+    themeColor: '#40c0cb',
+    msTileColor: '#313131',
+    iconPaths: {
+      favicon32: 'img/icons/favicon-32x32.png',
+      favicon16: 'img/icons/favicon-16x16.png',
+      appleTouchIcon: 'img/icons/apple-touch-icon.png'
+    }
+  }
+}
