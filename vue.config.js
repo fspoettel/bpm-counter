c0cd6737a44c810178bc285fecbe9343c2c085d6
Felix Spöttel <1682504+fspoettel@users.noreply.github.com>
2020-05-05 00:33:49 +0200

    Fix SW refreshs

--- vue.config.js
+++ vue.config.js
@@ -8,6 +8,10 @@ module.exports = {
       favicon32: 'img/icons/favicon-32x32.png',
       favicon16: 'img/icons/favicon-16x16.png',
       appleTouchIcon: 'img/icons/apple-touch-icon.png'
+    },
+    workboxOptions: {
+      skipWaiting: true,
+      clientsClaim: true
     }
   }
 }
